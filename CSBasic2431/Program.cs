ffc58709cad7413694237a1668db964a8aaf7c8c
wlgus123 <mail30019794@gmail.com>
2024-04-11 11:40:34 +0900

    초기 커밋.

---
+++ CSBasic2431/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSBasic2431
+{
+    internal static class Program
+    {
+        static void Main(string[] args)  // 메서드 첫글자는 무조건 대문자, string은 첫글자 소문자
+        {
+
+        }
+    }
+}
